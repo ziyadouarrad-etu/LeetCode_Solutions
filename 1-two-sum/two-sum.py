6465081ab0a0e7c0cf8b854aa21340ed6683e480
Ziyad Ouarrad <ziyad.ouarrad-etu@etu.univh2c.ma>
2025-11-14 12:13:18 +0100

    Time: 2079 ms (18.59%) | Memory: 13.3 MB (30.58%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -6,6 +6,6 @@ class Solution(object):
         :rtype: List[int]
         """
         for i in range(len(nums)):
-            for j in range(len(nums)):
-                if nums[i]+nums[j]==target and i!=j:
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
                     return [i,j]
