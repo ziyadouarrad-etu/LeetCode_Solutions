d3b79f9029fbb2bfe25fc53493c4afc642c476df
Ziyad Ouarrad <ziyad.ouarrad-etu@etu.univh2c.ma>
2025-11-16 19:49:55 +0100

    Time: 11 ms (45.05%) | Memory: 12.5 MB (13.10%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -7,21 +7,7 @@ class Solution(object):
         i=0
         total=0
         l=len(s)
-        while i<l:
-            if i<l-1:
-                current_val=self.roman(s[i])
-                next_val=self.roman(s[i+1])
-                if current_val<next_val:
-                    total+=(next_val-current_val)
-                    i+=2
-                    continue
-            total+=self.roman(s[i])
-            i+=1
-            f=0
-        return total
-    
-    def roman(self,s):
-        roman_dict={
+        roman={
             'I':1,
             'V':5,
             'X':10,
@@ -29,4 +15,15 @@ class Solution(object):
             'C':100,
             'D':500,
             'M':1000}
-        return roman_dict[s]
+        while i<l:                
+            current_val=roman[s[i]]
+            if i<l-1:
+                next_val=roman[s[i+1]]
+                if current_val<next_val:
+                    total+=(next_val-current_val)
+                    i+=2
+                    continue
+            total+=current_val
+            i+=1
+            f=0
+        return total
