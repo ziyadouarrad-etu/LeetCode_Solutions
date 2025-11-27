36c8eb5c1289e60c8bb06c90c2b19dd6ad76451c
Ziyad Ouarrad <ziyad.ouarrad-etu@etu.univh2c.ma>
2025-11-27 19:33:03 +0100

    Time: 2 ms (56.78%) | Memory: 7.8 MB (47.48%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.c
@@ -0,0 +1,14 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+int firstBadVersion(int n) {
+    long a = 1, b = n, c;
+    while (a < b) {
+        c = (a + b) / 2;
+        if (isBadVersion(c))
+            b = c;
+        else
+            a = c + 1;
+    }
+    return b;
+}
