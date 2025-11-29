94185ad50a39d2443ba396d73fcd5b909f7df5c3
Ziyad Ouarrad <ziyad.ouarrad-etu@etu.univh2c.ma>
2025-11-29 13:19:36 +0100

    Time: 0 ms (100.00%) | Memory: 12.5 MB (78.46%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.c
+++ 203-remove-linked-list-elements/remove-linked-list-elements.c
@@ -6,25 +6,16 @@
  * };
  */
 
-struct ListNode* createNode(int val) {
-    struct ListNode* res = malloc(sizeof(struct ListNode));
-    res->val = val;
-    res->next = NULL;
-    return res;
-}
 struct ListNode* removeElements(struct ListNode* head, int val) {
-    struct ListNode* newHead = NULL;
-    struct ListNode* curr = newHead;
-    struct ListNode* temp = NULL;
-    for (struct ListNode* p = head; p; p = p->next) {
-        if (p->val == val)
-            continue;
-        temp = createNode(p->val);
-        if (!newHead)
-            newHead = temp;
+    struct ListNode* curr = NULL;
+    while (head && head->val == val)
+        head = head->next;
+    curr = head;
+    while (curr && curr->next) {
+        if (curr->next->val == val)
+            curr->next = curr->next->next;
         else
-            curr->next = temp;
-        curr = temp;
+            curr = curr->next;
     }
-    return newHead;
+    return head;
 }
