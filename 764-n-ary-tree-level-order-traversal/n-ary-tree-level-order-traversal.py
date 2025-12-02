ca245cbbb0c4c61b139165898486d34e87835973
Ziyad Ouarrad <ziyad.ouarrad-etu@etu.univh2c.ma>
2025-12-02 16:46:13 +0100

    Time: 56 ms (17.87%) | Memory: 19.1 MB (97.02%) - LeetSync

---
+++ 764-n-ary-tree-level-order-traversal/n-ary-tree-level-order-traversal.py
@@ -0,0 +1,32 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+
+class Solution:
+    def levelOrder(self, root: "Node") -> List[List[int]]:
+        # Empty tree edge case
+        if not root:
+            return []
+
+        # Initialize lists that will be used as well as the result list
+        curr = [root]
+        childs = []
+        result = [[root.val]]
+
+        # Loop through all nodes in the current level getting there values and there children
+        while curr:
+            temp = []
+            for node in curr:
+                for child in node.children:
+                    childs.append(child)
+                    temp.append(child.val)
+            if temp:
+                result.append(temp)
+            curr = childs[:]
+            childs.clear()
+        return result
