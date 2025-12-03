51ba25b1fcd285fe3faf21b341611c8cb4f3a896
Ziyad Ouarrad <ziyad.ouarrad-etu@etu.univh2c.ma>
2025-12-03 11:31:42 +0100

    Time: 0 ms (100.00%) | Memory: 11.3 MB (46.90%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.c
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.c
@@ -8,8 +8,9 @@
  */
 int maxDepth(struct TreeNode* root) {
     int ld, rd;
-    if (!root)return 0;
-    ld=1+maxDepth(root->left);
-    rd=1+maxDepth(root->right);
-    return ld>rd?ld:rd;
+    if (!root)
+        return 0;
+    ld = 1 + maxDepth(root->left);
+    rd = 1 + maxDepth(root->right);
+    return ld > rd ? ld : rd;
 }
