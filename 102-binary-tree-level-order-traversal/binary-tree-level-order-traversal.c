6ca10bd7a3640409b2a27b4d2f7716c3e77e4d8d
Ziyad Ouarrad <ziyad.ouarrad-etu@etu.univh2c.ma>
2025-12-03 11:13:39 +0100

    Time: 0 ms (100.00%) | Memory: 13.2 MB (35.77%) - LeetSync

---
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.c
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** levelOrder(struct TreeNode* root, int* returnSize, int** returnColumnSizes) {
+        // Malloc necessary arrays
+    int** result = malloc(2000 * sizeof(int*));
+    int* columnSizes = malloc(2000 * sizeof(int));
+    *returnColumnSizes = columnSizes;
+
+    // Initialize *returnSize and handle empty tree edge case
+    *returnSize = 0;
+    if (!root)
+        return result;
+
+    /* In this solution we will use a queue to keep track of children
+    The logic is to that in each iteration we will have the current level nodes
+    in the queue, the when a node is treated (its val is gathered and all its
+    children added to end of queue), we will we will move forward to the next
+    node in the queue.
+    */
+    struct TreeNode** queue = malloc(2000 * sizeof(struct TreeNode*));
+    int head = 0, tail = 0, i, j, temp;
+    queue[tail++] = root;
+    while (head < tail) {
+        columnSizes[*returnSize] = tail - head;
+        result[*returnSize] = malloc(columnSizes[*returnSize] * sizeof(int));
+        temp = tail;
+        for (i = head; i < temp; i++) {
+            result[*returnSize][i - head] = queue[i]->val;
+            if (queue[i]->left)queue[tail++] = queue[i]->left;
+            if (queue[i]->right)queue[tail++] = queue[i]->right;
+        }
+        head = i;
+        (*returnSize)++;
+    }
+    return result;
+}
