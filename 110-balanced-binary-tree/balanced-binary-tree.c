fd4f65bcd4b7503deca08c5f67290de52dc1dd89
Ziyad Ouarrad <ziyad.ouarrad-etu@etu.univh2c.ma>
2025-12-04 11:15:29 +0100

    Time: 0 ms (100.00%) | Memory: 12.4 MB (32.48%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.c
+++ 110-balanced-binary-tree/balanced-binary-tree.c
@@ -8,22 +8,19 @@
  */
 #include <stdlib.h>
 
-int maxDepth(struct TreeNode* root) {
-    int ld, rd;
+int height(struct TreeNode* root) {
     if (!root)
-        return 0;
-    ld = 1 + maxDepth(root->left);
-    rd = 1 + maxDepth(root->right);
-    return ld > rd ? ld : rd;
+        return -1;
+    int left = height(root->left);
+    int right = height(root->right);
+    return 1 + (left > right ? left : right);
 }
 
 bool isBalanced(struct TreeNode* root) {
     if (!root)
         return true;
-    if (!root->left)
-        return maxDepth(root) <= 2;
-    if (!root->right)
-        return maxDepth(root) <= 2;
-    return abs(maxDepth(root->left) - maxDepth(root->right)) <= 1 &&
-               isBalanced(root->left) && isBalanced(root->right);
+    if (abs(height(root->left) - height(root->right)) > 1)
+        return false;
+
+    return isBalanced(root->left) && isBalanced(root->right);
 }
