2a5d7c9f422b852af864788e17aad5dcc6d9ef76
Ziyad Ouarrad <ziyad.ouarrad-etu@etu.univh2c.ma>
2025-12-06 17:50:51 +0100

    Time: 0 ms (100.00%) | Memory: 8.3 MB (27.43%) - LeetSync

--- 100-same-tree/same-tree.c
+++ 100-same-tree/same-tree.c
@@ -13,14 +13,9 @@ bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
         return false;
     if (p->val != q->val)
         return false;
-    if ((p->left && !q->left) || (p->right && !q->right))
-        return false;
-    if ((q->left && !p->left) || (q->right && !p->right))
-        return false;
-    bool bool1 = true, bool2 = true;
-    if (p->left)
-        bool1 = isSameTree(p->left, q->left);
-    if (p->right)
-        bool2 = isSameTree(p->right, q->right);
+
+    bool bool1, bool2;
+    bool1 = isSameTree(p->left, q->left);
+    bool2 = isSameTree(p->right, q->right);
     return bool1 && bool2;
 }
