ceaa49286ce855e0e7214781e9926dbbd450c48e
Ziyad Ouarrad <ziyad.ouarrad-etu@etu.univh2c.ma>
2025-12-06 17:49:47 +0100

    Time: 0 ms (100.00%) | Memory: 18.1 MB (12.59%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.py
+++ 101-symmetric-tree/symmetric-tree.py
@@ -12,15 +12,9 @@ class Solution:
             return False
         if p.val != q.val:
             return False
-        if (p.left and not q.left) or (p.right and not q.right):
-            return False
-        if (q.left and not p.left) or (q.right and not p.right):
-            return False
-        bool1, bool2 = True, True
-        if p.left:
-            bool1 = self.isSameTree(p.left, q.left)
-        if p.right:
-            bool2 = self.isSameTree(p.right, q.right)
+
+        bool1 = self.isSameTree(p.left, q.left)
+        bool2 = self.isSameTree(p.right, q.right)
         return bool1 and bool2
 
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
