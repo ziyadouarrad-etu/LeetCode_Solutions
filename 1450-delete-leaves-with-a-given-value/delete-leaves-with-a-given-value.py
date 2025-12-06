272e37bc735049f333327912150d805a6333f7fb
Ziyad Ouarrad <ziyad.ouarrad-etu@etu.univh2c.ma>
2025-12-06 13:26:01 +0100

    Time: 67 ms (8.59%) | Memory: 18.6 MB (7.46%) - LeetSync

--- 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
@@ -8,34 +8,37 @@ class Solution:
     def removeLeafNodes(
         self, root: Optional[TreeNode], target: int
     ) -> Optional[TreeNode]:
-    
+
         changed = True
         while changed:
+            if not root:
+                return None
+
+            # special case: root is a removable leaf
+            if not root.left and not root.right and root.val == target:
+                return None
+
             stack = [root]
             changed = False
             while stack:
                 node = stack.pop()
-                if (
-                    node.left
-                    and (not node.left.left and not node.left.right)
-                    and node.left.val == target
-                ):
-                    node.left = None
-                    changed = True
-                if (
-                    node.right
-                    and (not node.right.left and not node.right.right)
-                    and node.right.val == target
-                ):
-                    node.right = None
-                    changed = True
-                if node.left and node.right:
-                    stack.extend([node.left, node.right])
-                elif node.left:
-                    stack.append(node.left)
-                elif node.right:
-                    stack.append(node.right)
 
-        if not root or (not root.left and not root.right and root.val == target):
-            return None
+                if node.left:
+                    if (
+                        not node.left.left and not node.left.right
+                    ) and node.left.val == target:
+                        node.left = None
+                        changed = True
+                    else:
+                        stack.append(node.left)
+
+                if node.right:
+                    if (
+                        not node.right.left and not node.right.right
+                    ) and node.right.val == target:
+                        node.right = None
+                        changed = True
+                    else:
+                        stack.append(node.right)
+
         return root
