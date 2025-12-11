d9997191831997474ed0fdf9b1add1bd6fe50e19
Ziyad Ouarrad <ziyad.ouarrad-etu@etu.univh2c.ma>
2025-12-11 20:12:42 +0100

    Time: 567 ms (64.71%) | Memory: 60.2 MB (50.42%) - LeetSync

---
+++ 3819-count-covered-buildings/count-covered-buildings.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countCoveredBuildings(self, n: int, buildings: List[List[int]]) -> int:
+        result = 0
+        dx = {}
+        dy = {}
+
+        for building in buildings:
+            x, y = building[0], building[1]
+            if x not in dx:
+                dx[x] = []
+            dx[x].append(y)
+            if y not in dy:
+                dy[y] = []
+            dy[y].append(x)
+
+        for x, l in dx.items():
+            l.sort()
+            if len(l) > 2:
+                for y in l[1 : len(l) - 1]:
+                    dy[y].sort()
+                    result += (
+                        len(dy[y]) > 2 and dy[y][0] != x and dy[y][len(dy[y]) - 1] != x
+                    )
+        return result
